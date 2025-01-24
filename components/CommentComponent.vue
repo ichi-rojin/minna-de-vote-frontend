b452e5d3e32cce516d7f7a2354a26c4d6abe2677
ichi-rojin <rojin313@gmail.com>
2025-01-24 17:46:11 +0900

    Externalized unused pane

---
+++ components/CommentComponent.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="lg:col-span-2">
+    <div class="border-b pb-4 md:pb-6">
+      <h2 class="text-gray-800 text-lg lg:text-xl font-bold">有権者の声</h2>
+    </div>
+    <div class="divide-y">
+      <div class="flex flex-col gap-3 py-4 md:py-8">
+        <div>
+          <span class="block text-sm font-bold">まさぺこ</span>
+          <span class="block text-gray-500 text-sm">2021-07-10</span>
+        </div>
+        <p class="text-gray-600">坂井敏夫、胡散臭すぎ。違法献金どうなった？</p>
+      </div>
+      <div class="flex flex-col gap-3 py-4 md:py-8">
+        <div>
+          <span class="block text-sm font-bold">ダッシュ弾平</span>
+          <span class="block text-gray-500 text-sm">2021-07-08</span>
+        </div>
+        <p class="text-gray-600">
+          土井雅子は某国シンパです。こんな奴が当選したら日本終わります。
+        </p>
+      </div>
+      <div class="flex flex-col gap-3 py-4 md:py-8">
+        <div>
+          <span class="block text-sm font-bold">Greg Jackson</span>
+          <span class="block text-gray-500 text-sm">2021-07-05</span>
+        </div>
+        <p class="text-gray-600">
+          This is a section of some simple filler text, also known as
+          placeholder text. It shares some characteristics of a real written
+          text but is random or otherwise generated. It may be used to display a
+          sample of fonts or generate text for testing.
+        </p>
+      </div>
+    </div>
+    <div class="border-t pt-6">
+      <a
+        href="#"
+        class="flex items-center text-indigo-400 hover:text-indigo-500 active:text-indigo-600 font-semibold transition duration-100 gap-0.5"
+        >すべてのコメントを見る</a
+      >
+    </div>
+  </div>
+</template>
---
+++ components/SearchComponent.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="bg-white rounded-lg shadow sm:overflow-hidden mb-20">
+    <div class="px-4 py-8 sm:px-10">
+      <div class="relative mt-6">
+        <div class="absolute inset-0 flex items-center">
+          <div class="w-full border-t border-gray-300"></div>
+        </div>
+        <div class="relative flex justify-center text-sm leading-5">
+          <span class="px-2 text-gray-500 bg-white"> 選挙！を探す </span>
+        </div>
+      </div>
+      <div class="mt-6">
+        <div class="w-full space-y-6">
+          <div class="w-full">
+            <div class="relative">
+              <label for="candidate" class="inline-block text-xs mb-2">
+                候補者で探す
+              </label>
+              <input
+                id="search-form-price"
+                class="rounded-lg border-transparent flex-1 appearance-none border border-indigo-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                placeholder="日本 一"
+              />
+            </div>
+          </div>
+          <div class="w-full">
+            <div class="relative">
+              <label for="candidate" class="inline-block text-xs mb-2">
+                地域で探す
+              </label>
+              <input
+                id="search-form-location"
+                class="rounded-lg border-transparent flex-1 appearance-none border border-indigo-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                placeholder="大阪府"
+              />
+            </div>
+          </div>
+          <div class="w-full">
+            <div class="relative">
+              <label for="candidate" class="inline-block text-xs mb-2">
+                期日で探す
+              </label>
+              <input
+                type="date"
+                id="search-form-name"
+                class="rounded-lg border-transparent flex-1 appearance-none border border-indigo-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+              />
+            </div>
+          </div>
+          <div>
+            <span class="block w-full rounded-md shadow-sm">
+              <button
+                type="button"
+                class="py-2 px-4 bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg"
+              >
+                探す
+              </button>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="px-4 py-6 border-t-2 border-gray-200 bg-gray-50 sm:px-10">
+      <p class="text-xs leading-5 text-gray-500">
+        このデータは情報のために表示され、変更される可能性があります
+      </p>
+    </div>
+  </div>
+</template>
