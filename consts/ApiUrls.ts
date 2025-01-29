a0acf8f6037f29511621fe1a7af061b046c915ee
ichi-rojin <rojin313@gmail.com>
2025-01-29 15:35:31 +0900

    Separated API URLs

--- consts/ApiUrls.ts
+++ consts/ApiUrls.ts
@@ -1,7 +1,8 @@
+const BASE_URL = process.env.VUE_APP_URL;
 export class ApiUrls {
-  static readonly GET_LIST = "/api/list";
-  static readonly GET_DETAIL = "/api/detail";
-  static readonly GET_VOTE = "/api/vote";
-  static readonly POST_VOTE = "/api/vote";
-  static readonly POST_ELECTION = "/api/election";
+  static readonly GET_LIST = BASE_URL + "/api/list";
+  static readonly GET_DETAIL = BASE_URL + "/api/detail";
+  static readonly GET_VOTE = BASE_URL + "/api/vote";
+  static readonly POST_VOTE = BASE_URL + "/api/vote";
+  static readonly POST_ELECTION = BASE_URL + "/api/election";
 }
