77f8dae1da47a3b5824d1d84ac40f9530693f8a8
ichi-rojin <rojin313@gmail.com>
2025-03-06 14:28:15 +0900

    Remove unnecessary code

--- providers/detail/InjectedFetch.vue
+++ providers/detail/InjectedFetch.vue
@@ -1,15 +1,11 @@
 <script lang="ts" setup>
-import { defineExpose, onMounted } from "vue";
+import { onMounted } from "vue";
 import DetailKey from "./key";
 import injector from "@/providers/injector";
 import { useRoute } from "vue-router";
 
 const { fetch } = injector(DetailKey);
 const route = useRoute();
-
-defineExpose({
-  fetch,
-});
 
 onMounted(() => {
   fetch(route.params.id as string);
--- providers/list/InjectedFetch.vue
+++ providers/list/InjectedFetch.vue
@@ -1,12 +1,7 @@
 <script lang="ts" setup>
-import { defineExpose } from "vue";
 import ListKey from "./key";
 import injector from "@/providers/injector";
 
 const { fetch } = injector(ListKey);
-
-defineExpose({
-  fetch,
-});
 fetch();
 </script>
