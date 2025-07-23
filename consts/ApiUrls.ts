27a098910200d647d8b4a07c1f75120d8b09b084
ichi-rojin <rojin313@gmail.com>
2025-07-23 16:20:37 +0900

    Changed the API URL structure

--- consts/ApiUrls.ts
+++ consts/ApiUrls.ts
@@ -1,10 +1,10 @@
 const BASE_URL = process.env.VUE_APP_URL;
 export class ApiUrls {
-  static readonly POST_AUTH = BASE_URL + "/api/auth";
-  static readonly GET_LIST = BASE_URL + "/api/list";
-  static readonly GET_GROUP = BASE_URL + "/api/group";
-  static readonly GET_DETAIL = BASE_URL + "/api/detail";
-  static readonly GET_VOTE = BASE_URL + "/api/vote";
-  static readonly POST_VOTE = BASE_URL + "/api/vote";
-  static readonly POST_ELECTION = BASE_URL + "/api/election";
+  static readonly POST_AUTH = BASE_URL + "/auth";
+  static readonly GET_LIST = BASE_URL + "/list";
+  static readonly GET_GROUP = BASE_URL + "/group";
+  static readonly GET_DETAIL = BASE_URL + "/detail";
+  static readonly GET_VOTE = BASE_URL + "/vote";
+  static readonly POST_VOTE = BASE_URL + "/vote";
+  static readonly POST_ELECTION = BASE_URL + "/election";
 }
